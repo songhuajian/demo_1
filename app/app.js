fd46568c94fc3194b9dbf9e5e0fae18f5f6cb79d
unknown <>
2016-11-17 10:22:57 +0800

    最后

---
+++ app/app.js
@@ -0,0 +1,11 @@
+/*
+* @Author: 虚竹
+* @Date:   2016-09-29 09:29:36
+* @Last Modified by:   虚竹
+* @Last Modified time: 2016-09-29 10:44:40
+*/
+
+;(function(angular) {
+	'use strict';
+	var app = angular.module("renren", ['ngRoute']);
+})(angular);
